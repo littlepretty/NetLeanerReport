31fe5c8cdc588311de99a8bdfb25957f8fe45487
littlepretty@Mac <littlepretty881203@gmail.com>
2017-10-24 20:33:09 -0500

    init with existing doc
    Signed-off-by: littlepretty@Mac <littlepretty881203@gmail.com>

---
+++ Makefile
@@ -0,0 +1,31 @@
+PDFLATEX=pdflatex
+LATEX=latex
+BIBTEX=bibtex
+DVI2PDF=dvipdf
+PS2PDF=ps2pdf
+UNAME:=$(shell uname -s)
+ifeq ($(UNAME),Linux)
+	VIEWER=xdg-open
+endif
+ifeq ($(UNAME), Darwin)
+	VIEWER=open -a Preview
+endif
+
+NAME=main
+
+.PHONY: clean, view
+
+final:
+	$(PDFLATEX) $(NAME).tex
+	$(BIBTEX) $(NAME).aux
+	$(PDFLATEX) $(NAME).tex
+	$(PDFLATEX) $(NAME).tex
+
+view: final
+	$(VIEWER) $(NAME).pdf
+	rm *.log *.aux *.out *.blg *.bbl
+	#rm *-eps-converted-to.pdf
+	#pdffonts main.pdf
+
+clean:
+	rm *.log *.aux *.dvi *.out *.blg *.bbl *.ps *.bak
---
+++ figures/plot_accuracy.py
@@ -0,0 +1,35 @@
+from __future__ import print_function, division
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib
+
+matplotlib.rc('font', size=18)
+machines = ['MLP', 'RBM', 'SAE', 'DAE']
+accuracy = [81.4230, 77.5816, 79.1519, 76.9251]
+width = 0.4
+ind = np.arange(len(machines))
+
+fig, ax = plt.subplots()
+rects = ax.bar(ind, accuracy, width, color='b')
+ax.set_ylabel('Accuracy(%)')
+ax.set_xlabel('Classifiers')
+ax.set_xticks(ind + width / 2)
+ax.set_xticklabels(machines)
+ax.set_xlim([-0.5, len(machines)])
+ax.set_ylim([70, 84])
+plt.grid()
+
+
+def autolabel(rects):
+    """
+    Attach a text label above each bar displaying its height
+    """
+    for rect in rects:
+        height = rect.get_height()
+        ax.text(rect.get_x() + rect.get_width()/2., 1.005*height,
+                '%.2f' % height, ha='center', va='bottom')
+
+autolabel(rects)
+plt.tight_layout()
+plt.savefig('comp_accuracy.pdf', fmt='pdf')
+plt.show()
